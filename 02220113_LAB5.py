3b2afbdf00cae700dd5b57839d9238d076a83c49
Dell <02220113.cst@rub.edu.bt>
2025-04-14 14:42:42 +0600

    Part 1

---
+++ 02220113_LAB5.py
@@ -0,0 +1,20 @@
+def sequential_search(arr, target):
+    comparisons = 0
+    for index, value in enumerate(arr):
+        comparisons += 1
+        if value == target:
+            return index, comparisons
+    return -1, comparisons
+
+data = [23, 45, 12, 67, 89, 34, 56]
+target_value = 67
+
+print("List:", data)
+print(f"Searching for {target_value} using Sequential Search")
+index, comparisons = sequential_search(data, target_value)
+
+if index != -1:
+    print(f"Found at index {index}")
+else:
+    print("Not found")
+print(f"Number of comparisons: {comparisons}")
